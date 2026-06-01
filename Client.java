c04c84e9b9ccd165ba596da4dffb2cf451846ecc
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-06-01 19:27:06 +0530

    Learning user-defined packages

---
+++ Client.java
@@ -0,0 +1,21 @@
+//
+
+import PPA.Marvellous;
+import PPA.Infosystems;
+import PPA.LB.Pune;
+
+class Client
+{
+    public static void main(String A[])
+    {
+        Marvellous mobj = new Marvellous();
+        Infosystems iobj = new Infosystems();
+        Pune pobj = new Pune();
+
+        System.out.println("Inside main of Client");
+
+        mobj.fun();
+        iobj.gun();
+        pobj.sun();
+    }    
+}
