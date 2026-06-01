1e52370b2e4dbf02023b7030e5226f76a3bf5b9e
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-06-01 19:38:20 +0530

    Learning Super keyword

---
+++ SuperDemo.java
@@ -0,0 +1,49 @@
+class Base
+{
+    public int i;
+
+    public Base(int no)
+    {
+        System.out.println("Inside Base Construtor");
+        this.i = no;
+    }
+
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+}
+
+class Derived extends Base
+{
+    public int i;
+
+
+    public Derived()
+    {
+        System.out.println("Inside Derived construtor");
+        this.i = 21;
+    }
+
+    public void gun()
+    {
+        System.out.println("Inside Derived gun");
+        System.out.println("Value of i :"+i);
+        System.out.println("Value of i from Base :"+super.i);
+    }
+}
+
+class SuperDemo
+{
+    public static void main(String A[])
+    {
+        Derived dobj = new Derived();          //Error
+
+        dobj.gun();
+    }
+}
+
+//Compile : javac Single.java
+//Run : java Single
+
+//Compile + Run : java Single.java
