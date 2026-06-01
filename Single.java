b0466207a3b8f54b202d91a7502970da774b4df8
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-06-01 19:30:02 +0530

    Learning Inheritance in java

---
+++ Single.java
@@ -0,0 +1,68 @@
+class Base
+{
+    public int i;
+    public int j;
+
+    public Base()
+    {
+        System.out.println("Inside Base Construtor");
+        this.i = 0;
+        this.j = 0;
+    }
+
+    protected void finalize()
+    {
+        System.out.println("Inside finalize method of Base");
+    }
+
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+}
+
+class Derived extends Base
+{
+    public int x;
+
+
+    public Derived()
+    {
+        System.out.println("Inside Derived construtor");
+        this.x = 0;
+    }
+
+    protected void finalize()
+    {
+        System.out.println("Inside finalize method of Derived");
+    }
+
+    public void gun()
+    {
+        System.out.println("Inside Derived gun");
+    }
+}
+
+class Single
+{
+    public static void main(String A[])
+    {
+        System.out.println("Inside main");
+
+        Derived dobj = new Derived();
+
+        System.out.println(dobj.i);
+        System.out.println(dobj.j);
+        System.out.println(dobj.x);
+
+        dobj.fun();
+        dobj.gun();
+
+        System.out.println("End of main");
+    }
+}
+
+//Compile : javac Single.java
+//Run : java Single
+
+//Compile + Run : java Single.java
